3a3a96c4ff8e2b385aea27009a0fef72c57de01d
emmanuelangelo4199 <emmanuelmezie578@gmail.com>
2026-09-15 17:42:18 +0000

    refactor: Raise HTTPException (404) with a descriptive message when no   post is found, instead of manually setting response.status_code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
-from fastapi import Body, FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
+from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
 
@@ -15,18 +16,16 @@ class Post(BaseModel):
 
 
 my_posts = [
-            {"title": "post of posts 1", "content": "content of posts 1", "id": 1},
-            
-            {"title":"title of titles", "content":"contents of contents", "id":3},
-
-            {"title":"title of titles 2", "content":"contents of contents 2", "id":2},
+            {"Title": "post of posts 1", "content": "content of posts 1", "id": 1},
+            {"Title":"title of titles 2", "content":"contents of contents 2", "id":2},
+            {"Title":"title of titles", "content":"contents of contents", "id":3},
             
             ]
 
 # not the best way!!!!
 def find_post(id):
     for p in my_posts:
-        if p["id"] == id:
+        if p['id'] == id:
             return p
 
 @app.get("/")
@@ -38,7 +37,7 @@ def read_root():
 def read_item():
     return {"data":my_posts}
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):
 
     post_dict = post.model_dump()
@@ -46,10 +45,20 @@ def create_post(post: Post):
     post_dict ['id'] = randrange(0, 1000000)
 
     my_posts.append(post_dict)
-    return {"data ": post_dict}
+    return {"data": post_dict}
 
 @app.get ("/posts/{id}")
-def get_post(id):
+def get_post(id: int, response: Response):
     # converting the id to an integer to avoid a null output
-    post = find_post(int(id))
-    return {"get_post": post}
+    # print(type(id))
+    post = find_post(id)
+
+    if not post:
+
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"post with id: {id} was not found.")
+        
+        # not a good way for passing http status response
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {'E-message': f"post with id: {id} was not found."}
+
+    return {"post_details": post}
